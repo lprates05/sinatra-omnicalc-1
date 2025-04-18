10e1b74b6f264b7b987ac6e897b98668d3f56201
lprates05 <lorenapratesdl@outlook.com>
2025-04-18 15:58:50 +0000

    calculator

--- app.rb
+++ app.rb
@@ -1,13 +1,34 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/howdy") do
-erb(:hello)
+get("/") do
+  erb(:hello)
 end
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/square/new") do
+  erb(:square_new)
+end
+
+get("/square/results") do
+  @the_num = params.fetch("number").to_f
+  @the_result = @the_num **2
+  erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root_new)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("number").to_f
+  @the_result = @the_num ** (0.5)
+  erb(:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:payment_new)
+end
+
+get("/random/new") do
+  erb(:random_new)
 end
